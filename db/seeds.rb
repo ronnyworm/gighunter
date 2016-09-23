fe911f21af509532b49e0b744e67873d2b49b8bd
Ronny Worm <r-worm@gmx.de>
2016-09-23 14:07:51 +0200

    Es soll default Statuswerte geben

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Jede neue Band sollte diese Werte zunächst zur Verfügung gestellt bekommen, damit sie direkt beginnen kann, die Anwendung zu nutzen
+StatusValue.create(band_id: 1, text: "unbearbeitet", order: 1)
+StatusValue.create(band_id: 1, text: "Kontakt aufgenommen", order: 2)
+StatusValue.create(band_id: 1, text: "erledigt", order: 3)
