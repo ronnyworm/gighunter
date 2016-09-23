950a7ff5dec5c2fde4887aff516569a92095f6e2
Ronny Worm <r-worm@gmx.de>
2016-09-23 14:09:21 +0200

    Füge mehr Hilfsmethoden hinzu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :readable_datetime
+
+  def readable_datetime(dt)
+    dt.strftime("%Y-%m-%d %H:%M")
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,25 @@
 module ApplicationHelper
-	def readable_datetime(dt)
-		dt.strftime("%Y-%m-%d %H:%M")
+	def is_active?(caller)
+		if caller.include? "#"
+			caller_controller = caller.split("#")[0]
+			caller_actions = caller.split("#")[1].split("|")
+		elsif caller == controller_name
+			return true
+		end
+
+
+		# controller_name ist eine globale Variable von Rails
+		if caller_controller != controller_name
+			return false
+		end
+
+		# action_name ist eine globale Variable von Rails
+		caller_actions.each do |a|
+			if a == action_name
+				return true
+			end
+		end
+
+		return false
 	end
 end
