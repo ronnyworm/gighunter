1f21872c1a9e4b87afcf7a0b3b1cecdc16bfc28f
Ronny Worm <r-worm@gmx.de>
2016-09-25 15:39:08 +0200

    User Profil schön ändern lassen können

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
   def readable_datetime(dt)
     dt.strftime("%Y-%m-%d %H:%M")
   end
+
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :current_password) }
+  end
 end
